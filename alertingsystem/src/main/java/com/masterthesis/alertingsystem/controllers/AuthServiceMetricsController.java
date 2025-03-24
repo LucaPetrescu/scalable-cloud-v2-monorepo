14ef32749a6db19b2342ef0cebe12e9cd7266673
LucaPetrescu <luca@botag.no>
2025-03-24 10:08:31 +0200

    add new route to auth controller

--- alertingsystem/src/main/java/com/masterthesis/alertingsystem/controllers/AuthServiceMetricsController.java
+++ alertingsystem/src/main/java/com/masterthesis/alertingsystem/controllers/AuthServiceMetricsController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 @RequestMapping("/query-metrics")
 public class AuthServiceMetricsController {
@@ -22,17 +24,15 @@ public class AuthServiceMetricsController {
     }
 
     @GetMapping("/get-metric")
-    public ResponseEntity<MetricsResponseDto> getMetric(
-            @RequestParam(name = "serviceName", required = true)String serviceName,
-            @RequestParam(name = "metricName", required = true)String metricName
-    ) {
+    public ResponseEntity<MetricsResponseDto> getMetric(@RequestParam(name = "metricName", required = true) String metricName) {
+        MetricsResponseDto metricResponseDto = droolsRuleService.getMetric(metricName);
+        return new ResponseEntity<>(metricResponseDto, HttpStatus.OK);
+    }
 
-//        MetricsResponseDto metricResponseDto = droolsRuleService.getMetricForService(serviceName, metricName);
-
-//        return new ResponseEntity<>(metricResponseDto, HttpStatus.OK);
-
-        return null;
-
+    @GetMapping("/get-metrics")
+    public ResponseEntity<ArrayList<MetricsResponseDto>> getMetrics() {
+        ArrayList<MetricsResponseDto> metricsResponseDtoList = droolsRuleService.getAllMetrics();
+        return new ResponseEntity<>(metricsResponseDtoList, HttpStatus.OK);
     }
 
 }
