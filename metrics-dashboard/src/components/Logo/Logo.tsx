3dd8a31336001468f3abb6363f95eb5dbc1ccea6
LucaPetrescu <luca6011@gmail.com>
2025-05-21 01:41:47 +0300

    add functionality on dashboard

--- metrics-dashboard/src/components/Logo/Logo.tsx
+++ metrics-dashboard/src/components/Logo/Logo.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 export const Logo = () => {
-    return (
+  return (
     <div className="border-b mb-4 mt-2 pb-4 border-stone-300">
-        <button className="flex p-0.5 hover:bg-stone-200 rounded transition-colors relative gap-2 w-full items-center">
+      <button className="flex p-0.5 hover:bg-stone-200 rounded transition-colors relative gap-2 w-full items-center">
         <div className="text-start">
           <span className="text-sm font-bold block">ScalableCloudV2</span>
           <span className="text-xs block text-stone-500">Metrics Dashboard</span>
--- metrics-dashboard/src/components/Sidebar/Sidebar.tsx
+++ metrics-dashboard/src/components/Sidebar/Sidebar.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Logo } from '../Logo/Logo.tsx';
+import { Search } from '../Search/Search.tsx';
 export const Sidebar = () => {
     return <div>
-       <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
-        <Logo />
-       </div>
+        <div className="overflow-y-scroll sticky top-4 h-[calc(100vh-32px-48px)]">
+            <Logo />
+            <Search />
+        </div>
     </div>
 }
