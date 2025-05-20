2ba0f3bfaa29100c1ca5799cb7a8a5c76e974092
LucaPetrescu <luca6011@gmail.com>
2025-05-20 09:18:51 +0300

    add searchbar

--- metrics-dashboard/src/components/Search/Search.tsx
+++ metrics-dashboard/src/components/Search/Search.tsx
@@ -1,7 +1,15 @@
+import React from "react";
+
+import { FiCommand, FiSearch } from "react-icons/fi";
+
+
 export const Search = () => {
     return (
         <div>
+            <div className="bg-stone-200 mb-4 relative rounded flex items-center px-2 py-1.5 text-sm">
             <input type="text" placeholder="Search" />
         </div>
+        </div>
+        
     )
 }
